f8bb9e9feecb88ca9c8f7390deffd192c18e08ba
Donghoon Shin <ssshyhy@snu.ac.kr>
2022-07-16 23:30:56 +0900

    firebase added

--- HelloBot/AppDelegate.swift
+++ HelloBot/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore
 import IQKeyboardManagerSwift
 
 @main
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         IQKeyboardManager.shared.enable = true
         
         return true
